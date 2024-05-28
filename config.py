1d6a6e02950deb94f324445eea7d1d9740b7f31c
arrow <arashd1381@gmail.com>
2024-05-28 20:40:05 +0330

    added some changes

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# database
 DATABASE_URL = 'postgresql://test_db:123456@localhost:5532/test_db'
-
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 base = declarative_base()
--- models.py
+++ models.py
@@ -7,5 +7,5 @@ class User(base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
-    username = Column(String)
+    username = Column(String, unique=True)
     hashed_password = Column(String)
