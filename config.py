390456314a7b245bd49910a8aeebc2e2ff14abba
arrow <arashd1381@gmail.com>
2024-06-03 18:44:21 +0330

    added default values in database config

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # database
-database_location = os.environ.get('DATABASE_LOCATION')
-username = os.environ.get('DATABASE_USERNAME')
-password = os.environ.get('DATABASE_PASSWORD')
-database_name = os.environ.get('DATABASE_NAME')
-DATABASE_URL = f'postgresql://{username}:{password}@db/{database_name}'
+database_location = os.environ.get('DATABASE_LOCATION') or "localhost:5532"
+username = os.environ.get('DATABASE_USERNAME') or "test_db"
+password = os.environ.get('DATABASE_PASSWORD') or "123456"
+database_name = os.environ.get('DATABASE_NAME') or "test_db"
+DATABASE_URL = f'postgresql://{username}:{password}@{database_location}/{database_name}'
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 base = declarative_base()
